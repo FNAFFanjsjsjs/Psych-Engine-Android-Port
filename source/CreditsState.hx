ba5ced23ba0d84dd94229bb5fd1aaaedbb3258d5
FNAFFanjsjsjs <87219330+FNAFFanjsjsjs@users.noreply.github.com>
2021-12-05 21:07:59 +0800

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,9 +46,9 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Android Port'],
-			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
-			['Psych Engine Team'],
+			['idk test probably since IDK how this work'],
+			['helpy',		    'majigsaw',		    'this is completely a test',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['uwu'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
 			[''],
