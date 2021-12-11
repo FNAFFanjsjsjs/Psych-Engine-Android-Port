ab14cffa0c709822667a1d4bbd5a2140b3c31cde
FNAFFanjsjsjs <87219330+FNAFFanjsjsjs@users.noreply.github.com>
2021-12-11 10:36:18 +0800

    Test
    Test

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -20,13 +20,13 @@ class Achievements {
 		["Highscore!!",					"Beat Week 6 on Hard with no Misses.",				'week6_nomiss',			false],
 		["You'll Pay For That...",		"Beat Week 7 on Hard with no Misses.",				'week7_nomiss',			 true],
 		["What a Funkin' Disaster!",	"Complete a Song with a rating lower than 20%.",	'ur_bad',				false],
-		["Perfectionist",				"Complete a Song with a rating of 100%.",			'ur_good',				false],
-		["Roadkill Enthusiast",			"Watch the Henchmen die over 100 times.",			'roadkill_enthusiast',	false],
-		["Oversinging Much...?",		"Hold down a note for 20 seconds.",					'oversinging',			false],
-		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					false],
+		["Perfectionist",				"Complete a Song with a rating of 100%.",			'ur_good',				true],
+		["Roadkill Enthusiast",			"Watch the Henchmen die over 100 times.",			'roadkill_enthusiast',true],
+		["Oversinging Much...?",		"Hold down a note for 20 seconds.",					'oversinging',			true],
+		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					true],
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
-		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
-		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				true],
+		["Kyla Ternate",					"Kyla de negra.",	'Kyla Ternate',				 true]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
